f4bedc953af361ff83e4a0520315249ef4c5bfe3
Irfan <irfanft12@gmail.com>
2021-05-28 17:59:48 +0530

    Next First Commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is a Musical App</p>
+            <p>The version of app is 1.0.0</p>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEvents() {
+    return (
+        <div>
+            <h1>Add Events</h1>
+        </div>
+    )
+}
