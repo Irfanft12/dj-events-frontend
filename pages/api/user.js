af6aef64b539665a28fa24bf0657fd36cb6abbda
Irfanft12 <irfanft12@gmail.com>
2021-06-14 14:00:39 +0530

    no cors mode added

--- pages/api/user.js
+++ pages/api/user.js
@@ -11,6 +11,7 @@ export default async (req, res) => {
         const {token} = cookie.parse(req.headers.cookie)
 
         const strapiRes = await fetch(`${API_URL}/users/me`, {
+            mode: 'no-cors',
             method: "GET",
             headers: {
                 Authorization: `Bearer ${token}`
